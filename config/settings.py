75fc3540919f043b99d037b9c35b9bd64f053c92
GitLukeW <gitlukew@gmail.com>
2023-01-13 18:18:23 -0500

    fixed key error

--- config/settings.py
+++ config/settings.py
@@ -30,7 +30,7 @@ environ.Env.read_env()
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = []
 
