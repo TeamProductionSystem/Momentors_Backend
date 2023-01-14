4155725cf8ba5e43e02ab633ed0fea0dd2c7e584
GitLukeW <gitlukew@gmail.com>
2023-01-14 17:53:58 -0500

    setting up render

--- team_production_system/models.py
+++ team_production_system/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+class User(AbstractUser):
+    first_name = models.CharField(max_length=50, null=True, blank=True)
+    last_name = models.CharField(max_length=50, null=True, blank=True) 
+    photo = models.ImageField(upload_to="user_photo", blank=True, null=True)
+    employee_id = models.CharField(max_length=50, null=True, blank=True)
+
+    def __str__(self):
+        return self.first_name
