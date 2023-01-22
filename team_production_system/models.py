8c0f8f1d7289c3f55a7d74a69f47912fccf3a757
GitLukeW <gitlukew@gmail.com>
2023-01-21 20:27:12 -0500

    setup activity name and number

--- team_production_system/models.py
+++ team_production_system/models.py
@@ -3,11 +3,23 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 
+
 class User(AbstractUser):
     first_name = models.CharField(max_length=50, null=True, blank=True)
-    last_name = models.CharField(max_length=50, null=True, blank=True) 
+    last_name = models.CharField(max_length=50, null=True, blank=True)
     photo = models.ImageField(upload_to="user_photo", blank=True, null=True)
     employee_id = models.CharField(max_length=50, null=True, blank=True)
 
     def __str__(self):
         return self.first_name
+
+
+class Activities(models.Model):
+    activity_name = models.TextField(max_length=250)
+    activity_number = models.IntegerField()
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='activity_number')
+
+
+    def __str__(self):
+        return f'{self.activity_name}, {self.activity_number}'
