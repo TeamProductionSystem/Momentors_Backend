9a3d60ba2c1367d1a602b302ce432de1d359c36e
GitLukeW <gitlukew@gmail.com>
2023-02-07 11:46:09 -0500

    working on djoser

--- team_production_system/urls.py
+++ team_production_system/urls.py
@@ -2,11 +2,11 @@ from django.urls import path, include
 from .views import UserCreateView, MentorViewSet, MenteeViewSet
 from rest_framework import routers
 
-router = routers.DefaultRouter()
-router.register(r'mentors', MentorViewSet)
-router.register(r'mentees', MenteeViewSet)
+# router = routers.DefaultRouter()
+# router.register(r'mentors', MentorViewSet)
+# router.register(r'mentees', MenteeViewSet)
 
 urlpatterns = [
     path('register/', UserCreateView.as_view(), name='register'),
-    path('', include(router.urls)),
+    # path('', include(router.urls)),
 ]
--- team_production_system/views.py
+++ team_production_system/views.py
@@ -3,18 +3,18 @@ from rest_framework import generics
 from .models import User, Mentor, Mentee
 from .serializers import UserSerializer, MentorSerializer, MenteeSerializer
 
-class UserCreateView(UserCreateView):
-    serializer_class = UserSerializer
+# class CustomUserCreateView(UserCreateView):
+#     serializer_class = UserSerializer
 
-    def perform_create(self, serializer):
-        user = serializer.save()
-        is_mentor = self.request.data.get('is_mentor')
-        is_mentee = self.request.data.get('is_mentee')
+#     def perform_create(self, serializer):
+#         user = serializer.save()
+#         is_mentor = self.request.data.get('is_mentor')
+#         is_mentee = self.request.data.get('is_mentee')
 
-        if is_mentor:
-            Mentor.objects.create(user=user)
-        elif is_mentee:
-            Mentee.objects.create(user=user)
+#         if is_mentor:
+#             Mentor.objects.create(user=user)
+#         elif is_mentee:
+#             Mentee.objects.create(user=user)
 
 class MentorViewSet(generics.ListAPIView):
     queryset = Mentor.objects.all()
