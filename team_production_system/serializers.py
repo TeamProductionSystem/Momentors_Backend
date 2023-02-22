9aca5c7c9559c22fa65f0cb77ade77a50c040b4d
GitLukeW <gitlukew@gmail.com>
2023-02-22 17:15:52 -0500

    Haveing an issue with the photo not uploading on insomnia

--- team_production_system/serializers.py
+++ team_production_system/serializers.py
@@ -2,32 +2,6 @@ from rest_framework import serializers
 from .models import Mentor, Mentee, SessionRequestForm, CustomUser
 
 
-# class CustomUserSerializer(serializers.ModelSerializer):
-#     profile_photo = serializers.ImageField()
-
-#     class Meta:
-#         model = CustomUser
-#         fields = [
-#             'pk',
-#             'username',
-#             'first_name',
-#             'last_name',
-#             'email',
-#             'phone_number',
-#             'profile_photo',
-#             'is_mentor',
-#             'is_mentee',
-#             'is_active',
-#         ]
-
-#     def update(self, instance, validated_data):
-#         profile_photo = validated_data.get('profile_photo', None)
-#         if profile_photo:
-#             instance.profile_photo = profile_photo
-#             instance.save()
-#         return instance
-
-
 class CustomUserSerializer(serializers.ModelSerializer):
     profile_photo = serializers.ImageField()
 
@@ -47,16 +21,10 @@ class CustomUserSerializer(serializers.ModelSerializer):
         ]
 
     def update(self, instance, validated_data):
-        # Extract the file data from the validated data
-        profile_photo = validated_data.pop('profile_photo', None)
-        
-        # Call the parent update method to handle non-file fields
-        instance = super().update(instance, validated_data)
-
-        # If a new file was provided, create a new file instance and assign it to the model field
+        profile_photo = validated_data.get('profile_photo', None)
         if profile_photo:
-            instance.profile_photo.save(profile_photo.name, profile_photo)
-        
+            instance.profile_photo = profile_photo
+            instance.save()
         return instance
 
 
