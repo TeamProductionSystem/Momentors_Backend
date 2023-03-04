c8f7f6fde303b7ba601fc9afae58a1123462b09c
GitLukeW <gitlukew@gmail.com>
2023-03-03 20:10:00 -0500

    comments

--- team_production_system/urls.py
+++ team_production_system/urls.py
@@ -3,11 +3,16 @@ from team_production_system import views
 
 
 urlpatterns = [
+    # User information end points
+     path('user/<int:pk>/', views.CustomUserView.as_view(), name='user'),
+     path('myprofile/', views.UserProfile.as_view(), name='my-profile'),
+
+     # List of mentor and mentees end points
      path('mentor/', views.MentorList.as_view(), name='mentor-list'),
      path('mentee/', views.MenteeList.as_view(), name='mentee-list'),
-     path('sessionrequestform/', views.SessionRequestForm.as_view(), name='request-form'),
-     path('user/<int:pk>/', views.CustomUserView.as_view(), name='user'),
+
+     # End points related to sessions
      path('availabilty/', views.AvailabilityView.as_view(), name='availabilty'),
+     path('sessionrequestform/', views.SessionRequestForm.as_view(), name='request-form'),
      path('session/', views.SessionView.as_view(), name='session'),
-     path('myprofile/', views.UserProfile.as_view(), name='my-profile'),
 ]
