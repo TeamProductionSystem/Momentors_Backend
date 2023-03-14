f2f0035728209587f04308855ad788928f1e626c
GitLukeW <gitlukew@gmail.com>
2023-03-14 13:08:53 -0400

    adding endpoint info to readme

--- team_production_system/views.py
+++ team_production_system/views.py
@@ -1,13 +1,12 @@
-from .models import CustomUser, Mentee, SessionRequestForm, Availability, Session, Mentor
+from .models import CustomUser, Mentee, SessionRequestForm, Availability
+from .models import Session, Mentor
 from rest_framework import generics, status
-from .serializers import CustomUserSerializer, SessionRequestSerializer, AvailabilitySerializer, SessionSerializer, MentorListSerializer, MenteeListSerializer, MentorProfileSerializer, MenteeProfileSerializer
-from django.db.models import Q
-from rest_framework.parsers import MultiPartParser, FormParser
+from .serializers import CustomUserSerializer, AvailabilitySerializer
+from .serializers import SessionRequestSerializer, SessionSerializer
+from .serializers import MentorListSerializer, MentorProfileSerializer
+from .serializers import MenteeListSerializer, MenteeProfileSerializer
 from rest_framework.response import Response
-from django.shortcuts import get_object_or_404
 from django.utils import timezone
-import datetime
-from datetime import timedelta
 from rest_framework.permissions import IsAuthenticated
 
 
@@ -73,9 +72,8 @@ class MentorInfoUpdateView(generics.RetrieveUpdateDestroyAPIView):
     def get_object(self):
         return self.request.user.mentor
 
+
 # View to see a list of all users flagged as a mentee
-
-
 class MenteeList(generics.ListAPIView):
     permission_classes = [IsAuthenticated]
 
@@ -83,14 +81,28 @@ class MenteeList(generics.ListAPIView):
         try:
             queryset = CustomUser.objects.filter(is_mentee=True)
         except Exception as e:
-            return Response({"error": "Failed to retrieve mentee list."}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+            return Response({"error": "Failed to retrieve mentee list."},
+                            status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
         if not queryset:
-            return Response({"message": "No mentees found."}, status=status.HTTP_404_NOT_FOUND)
+            return Response({"message": "No mentees found."},
+                            status=status.HTTP_404_NOT_FOUND)
 
         serializer = MenteeListSerializer(queryset, many=True)
         response_data = serializer.data
         return Response(response_data)
+
+
+# View to allow mentees to create and view the about me/skills.
+class MenteeInfoView(generics.ListCreateAPIView):
+    serializer_class = MenteeProfileSerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+    def get_queryset(self):
+        return Mentee.objects.filter(user=self.request.user)
 
 
 # View to edit the logged in mentees team number
@@ -105,7 +117,47 @@ class MenteeInfoUpdateView(generics.RetrieveUpdateDestroyAPIView):
         return self.request.user.mentee
 
 
-# View for mentees to submit session request forms. It is set to auto add the user who submited the form as request user.
+# Create and view all availabilities
+class AvailabilityView(generics.ListCreateAPIView):
+    serializer_class = AvailabilitySerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        # Exclude any availability that has an end time in the past
+        return Availability.objects.filter(end_time__gte=timezone.now())
+
+    def get(self, request):
+        try:
+            availabilities = self.get_queryset()
+
+            # Check if there are any availabilities
+            if not availabilities:
+                return Response("No open availabilities.",
+                                status=status.HTTP_404_NOT_FOUND)
+
+            serializer = self.serializer_class(availabilities, many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        except:
+            return Response("Error: Failed to retrieve availabilities.",
+                            status=status.HTTP_400_BAD_REQUEST)
+
+    def post(self, request):
+        try:
+            serializer = self.serializer_class(data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data,
+                                status=status.HTTP_201_CREATED)
+            else:
+                return Response(serializer.errors,
+                                status=status.HTTP_400_BAD_REQUEST)
+        except:
+            return Response("Error: Failed to create availability.",
+                            status=status.HTTP_400_BAD_REQUEST)
+
+
+# View for mentees to submit session request forms.
+# It is set to auto add the user who submited the form as request user.
 class SessionRequestForm(generics.ListCreateAPIView):
     queryset = SessionRequestForm.objects.all()
     serializer_class = SessionRequestSerializer
@@ -126,53 +178,8 @@ class SessionRequestForm(generics.ListCreateAPIView):
                             status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 
-# Create and view all availabilities
-class AvailabilityView(generics.ListCreateAPIView):
-    serializer_class = AvailabilitySerializer
-    permission_classes = [IsAuthenticated]
-
-    def get_queryset(self):
-        # Exclude any availability that has an end time in the past
-        return Availability.objects.filter(end_time__gte=timezone.now())
-
-    def get(self, request):
-        try:
-            availabilities = self.get_queryset()
-
-            # Check if there are any availabilities
-            if not availabilities:
-                return Response("No open availabilities.", status=status.HTTP_404_NOT_FOUND)
-
-            serializer = self.serializer_class(availabilities, many=True)
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        except:
-            return Response("Error: Failed to retrieve availabilities.", status=status.HTTP_400_BAD_REQUEST)
-
-    def post(self, request):
-        try:
-            serializer = self.serializer_class(data=request.data)
-            if serializer.is_valid():
-                serializer.save()
-                return Response(serializer.data, status=status.HTTP_201_CREATED)
-            else:
-                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        except:
-            return Response("Error: Failed to create availability.", status=status.HTTP_400_BAD_REQUEST)
-
-
 # Creat and view all sessions
 class SessionView(generics.ListCreateAPIView):
     queryset = Session.objects.all()
     serializer_class = SessionSerializer
     permission_classes = [IsAuthenticated]
-
-
-class MenteeInfoView(generics.ListCreateAPIView):
-    serializer_class = MenteeProfileSerializer
-    permission_classes = [IsAuthenticated]
-
-    def perform_create(self, serializer):
-        serializer.save(user=self.request.user)
-
-    def get_queryset(self):
-        return Mentee.objects.filter(user=self.request.user)
