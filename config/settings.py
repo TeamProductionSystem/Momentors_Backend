2b7c5e4d62728a1d4f3216522c9beee47d78d16c
GitLukeW <gitlukew@gmail.com>
2023-03-17 12:36:34 -0400

    added 30001

--- config/settings.py
+++ config/settings.py
@@ -177,7 +177,9 @@ CORS_ALLOWED_ORIGINS = [
     "https://team-production-system.onrender.com",
     "http://localhost:8080",
     "http://127.0.0.1:8000",
-    "http://localhost:3000", 
+    "http://localhost:3000",
+    "http://localhost:3001",
+
 
 ]
 
