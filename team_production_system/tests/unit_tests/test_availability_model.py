7f569a9bab609151a98f33aabfe5631cca932400
Em Sparr <em.c.sparr@gmail.com>
2023-06-27 19:04:55 -0400

    test: adds availability model unit test

---
+++ team_production_system/tests/unit_tests/test_availability_model.py
@@ -0,0 +1,37 @@
+from django.test import TestCase
+from datetime import datetime, timedelta, timezone
+from ..models import Availability, Mentor, CustomUser
+
+
+class AvailabilityTestCase(TestCase):
+    def setUp(self):
+        # Create a User and Mentor object
+        self.user = CustomUser.objects.create_user(
+            username='mentor',
+            email='mentor@example.com',
+            password='password'
+        )
+        self.mentor = Mentor.objects.create(user=self.user)
+
+    def test_create_availability(self):
+        # Set start and end times
+        start_time = datetime.now(timezone.utc)
+        end_time = start_time + timedelta(hours=1)
+
+        # Create an Availability object
+        availability = Availability(
+            mentor=self.mentor,
+            start_time=start_time,
+            end_time=end_time)
+
+        # Save an Availability object associated with the Mentor
+        availability.save()
+
+        # Retrieve the saved Availability object
+        saved_availability = Availability.objects.first()
+
+        # Check that the object was saved correctly
+        self.assertEqual(saved_availability.start_time, start_time)
+        self.assertEqual(saved_availability.end_time, end_time)
+        self.assertEqual(saved_availability.mentor, self.mentor)
+        self.assertEqual(saved_availability.pk, 1)
