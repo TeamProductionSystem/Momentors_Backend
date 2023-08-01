8c3ec386a8cab78a1007f2714e8786d401a00673
Nathan Zeager <nathanzeager@hotmail.com>
2023-08-01 16:48:02 -0400

    tasks.py now triggers an email for 15/60 minute notifications

--- team_production_system/tasks.py
+++ team_production_system/tasks.py
@@ -2,20 +2,21 @@ from celery import shared_task
 from datetime import datetime, timedelta
 from django.utils import timezone
 from .models import Session
+from rest_framework.response import Response
+from rest_framework import status
 
 
 @shared_task
-# Redis will run this every 5 (?) minutes
+# Redis will run this every 5 minutes
 def notify(session_pk):
-    now = datetime.now()
+    now = datetime.now(timezone.utc)
     session = Session.objects.get(pk=session_pk)
     start_time = session.start_time
-    session.mentor_confirm_notify()
 
-    # We may need to mess with the logic if the server only runs every 5 min to check
+    # We check a range of times since the plan is to have Redis run every 5 minutes
     if start_time - timedelta(minutes=60) <= now <= start_time - timedelta(minutes=55):
-        if session.user.notification_settings.sixty_minute_alert:
+        if session.mentor.user.notification_settings.sixty_minute_alert:
             session.sixty_min_notify()
     if start_time - timedelta(minutes=15) <= now <= start_time - timedelta(minutes=10):
-        if session.user.notification_settings.fifteen_minute_alert:
+        if session.mentor.user.notification_settings.fifteen_minute_alert:
             session.fifteen_min_notify()
