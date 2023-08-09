30cfe504686c622df8b1a1ebcdb30965ca33df63
Em Sparr <em.c.sparr@gmail.com>
2023-08-09 13:05:09 -0400

    test: adds notify task testing

--- team_production_system/tasks.py
+++ team_production_system/tasks.py
@@ -2,8 +2,6 @@ from celery import shared_task
 from datetime import datetime, timedelta
 from django.utils import timezone
 from .models import Session
-from rest_framework.response import Response
-from rest_framework import status
 
 
 @shared_task
@@ -15,10 +13,14 @@ def notify():
     for session in sessions:
         start_time = session.start_time
 
-        # We check a range of times since the plan is to have Redis run every 5 minutes
-        if start_time - timedelta(minutes=60) <= now <= start_time - timedelta(minutes=55):
+        # We check a range of times to have Redis run every 5 minutes
+        if start_time - timedelta(minutes=60) \
+            <= now \
+                <= start_time - timedelta(minutes=55):
             if session.mentor.user.notification_settings.sixty_minute_alert:
                 session.sixty_min_notify()
-        elif start_time - timedelta(minutes=15) <= now <= start_time - timedelta(minutes=10):
+        elif start_time - timedelta(minutes=15) \
+            <= now \
+                <= start_time - timedelta(minutes=10):
             if session.mentor.user.notification_settings.fifteen_minute_alert:
                 session.fifteen_min_notify()
