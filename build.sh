3a8ad53f60b3940802fa9b23b83e6a90087dab3d
GitLukeW <gitlukew@gmail.com>
2023-08-19 14:52:09 -0400

    update build.sh

--- build.sh
+++ build.sh
@@ -8,6 +8,6 @@ pipenv run python manage.py migrate
 pipenv run python manage.py collectstatic --no-input
 pipenv run python manage.py add_superuser
 
-celery --app config.celery_settings worker --loglevel info --concurrency 4
+celery --app config worker --loglevel info --concurrency 4
 celery -A config.celery_settings beat -l debug
 
