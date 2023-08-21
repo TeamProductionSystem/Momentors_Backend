ce97eead2896798d021d31e9ebd9ff1c33e2fb3a
GitLukeW <gitlukew@gmail.com>
2023-08-21 18:37:54 -0400

    hotfix celery

--- build.sh
+++ build.sh
@@ -8,6 +8,6 @@ pipenv run python manage.py migrate
 pipenv run python manage.py collectstatic --no-input
 pipenv run python manage.py add_superuser
 
-celery --app team_production_system.tasks worker --loglevel info --concurrency 4
+celery -A config.celery_settings worker --loglevel info --concurrency 4
 celery -A config.celery_settings beat -l debug
 
