e52e332224b84ad2bb1cc8d94cee6a6057bfa8cb
Em Sparr <em.c.sparr@gmail.com>
2023-09-06 18:54:27 -0400

    chore: cleans up all linting errors

--- team_production_system/tests/unit_tests/models/test_availability_model.py
+++ team_production_system/tests/unit_tests/models/test_availability_model.py
@@ -26,7 +26,7 @@ class AvailabilityTestCase(TestCase):
 
         # Save an Availability object associated with the Mentor
         availability.save()
-        
+
         # Retrieve the saved Availability object
         saved_availability = Availability.objects.first()
 
@@ -34,6 +34,6 @@ class AvailabilityTestCase(TestCase):
         self.assertEqual(saved_availability.start_time, start_time)
         self.assertEqual(saved_availability.end_time, end_time)
         self.assertEqual(saved_availability.mentor, self.mentor)
-        self.assertEqual(str(saved_availability), f"{self.mentor} is available from {start_time} to {end_time}.")
-
-    # TODO: Add test for self reference
+        self.assertEqual(str(saved_availability),
+                         f"{self.mentor} is available from "
+                         f"{start_time} to {end_time}.")
