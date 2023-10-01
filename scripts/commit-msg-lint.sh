4439dfccb62ec1495895ecfc95d84d2693a9af50
GitLukeW <102833183+GitLukeW@users.noreply.github.com>
2023-10-01 16:07:31 -0400

    chore(precommit): setup precommit (#149)
    * chore(precommit): setup precommit
    
    * chore(precommit): setup precommit
    
    * chore(precommit): setup precommit
    
    * chore(precommit): update readme

---
+++ scripts/commit-msg-lint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+commit_msg_file=$1
+commit_msg=$(cat $commit_msg_file)
+
+regex="^(feat|fix|docs|style|refactor|perf|test|build|ci|chore|revert)\([a-z]+\): .+$"
+
+if [[ ! $commit_msg =~ $regex ]]; then
+    echo "Error: Commit message does not match the required format!"
+     echo "Expected Format: <category(scope): description>"
+    echo "Example: chore(test): setup test"
+    exit 1
+fi
