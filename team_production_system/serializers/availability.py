4398aa0796ef814fad04b2e7d3c4fc4ddaaed8b9
Em Sparr <em.c.sparr@gmail.com>
2023-10-04 01:54:20 -0400

    refactor(availability): breaks up 30 mins chunks in serializer

--- team_production_system/serializers/availability.py
+++ team_production_system/serializers/availability.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from django.utils import timezone
+from datetime import timedelta
 from team_production_system.models import (
     Mentor,
     Availability,
@@ -19,6 +20,23 @@ class AvailabilitySerializer(serializers.ModelSerializer):
             user=self.context['request'].user)
         start_time = validated_data['start_time']
         end_time = validated_data['end_time']
+
+        availability_overlap = self.check_availability_overlap(
+            mentor, start_time, end_time)
+
+        if not availability_overlap:
+
+            availabilities = self.create_30_min_availabilities(
+                start_time, end_time, mentor)
+
+            Availability.objects.bulk_create(availabilities)
+
+            return validated_data
+
+        raise serializers.ValidationError(
+            "Input overlaps with existing availability.")
+
+    def check_availability_overlap(self, mentor, start_time, end_time):
         # Check if start time is between a start time and end time of
         # an existing availability
         overlapping_condition1 = Availability.objects.filter(
@@ -51,22 +69,36 @@ class AvailabilitySerializer(serializers.ModelSerializer):
             or overlapping_condition2
             or overlapping_condition3
             or overlapping_condition4)
-        if not availability_overlap:
-            availability = Availability.objects.create(
-                mentor=mentor, **validated_data)
-            return availability
-        raise serializers.ValidationError(
-            "Input overlaps with existing availability.")
+        return availability_overlap
+
+    def create_30_min_availabilities(self, start_time, end_time, mentor):
+        chunk_size = timedelta(minutes=30)
+
+        # Create a list of Availability objects for each time chunk
+        availabilities = []
+        while start_time < end_time:
+            availability = Availability(
+                mentor=mentor,
+                start_time=start_time,
+                end_time=start_time + chunk_size,
+            )
+            availabilities.append(availability)
+            start_time += chunk_size
+        return availabilities
 
     def validate(self, data):
         """
-        Check that the start_time is before the end_time.
+        Check that the start_time is before the end_time and in future.
         """
         start_time = data['start_time']
         end_time = data['end_time']
         if start_time >= end_time:
             raise serializers.ValidationError(
                 'End time must be after start time.')
+        if start_time <= timezone.now():
+            raise serializers.ValidationError(
+                'Start time must be in the future.'
+            )
         return data
 
     def validate_end_time(self, value):
@@ -78,5 +110,3 @@ class AvailabilitySerializer(serializers.ModelSerializer):
                 'End time must be in the future.'
             )
         return value
-
-    # TODO: Add validation for start times
