14c90b8cd10cded67aba65304de34efd560eaec4
GitLukeW <gitlukew@gmail.com>
2023-10-07 18:33:49 -0400

    feat(tests): setup test action fix

--- team_production_system/tests/unit_tests/models/test_mentor_model.py
+++ team_production_system/tests/unit_tests/models/test_mentor_model.py
@@ -5,7 +5,6 @@ from ....models import Mentor, CustomUser
 class MentorModelTest(TestCase):
 
     def setUp(self):
-        # Assuming CustomUser has a username and password
         self.user = CustomUser.objects.create(username='testuser',
                                               password='password123')
 
@@ -17,8 +16,6 @@ class MentorModelTest(TestCase):
             skills=['HTML', 'CSS', 'Django']
         )
 
-        # Assert that the mentor object was saved and has the correct
-        # attributes
         self.assertEqual(mentor.about_me, 'This is a test about me.')
         self.assertEqual(mentor.team_number, 5)
         self.assertListEqual(list(mentor.skills), ['HTML', 'CSS', 'Django'])
@@ -31,25 +28,20 @@ class MentorModelTest(TestCase):
             skills=['HTML']
         )
 
-        # Assert that the __str__ method returns the correct representation
         self.assertEqual(str(mentor), 'testuser')
 
     def test_default_about_me(self):
-        # Create a Mentor object without specifying an 'about_me'
         mentor = Mentor.objects.create(
             user=self.user,
             skills=['HTML']
         )
 
-        # Assert that the default value for 'about_me' is set
         self.assertEqual(mentor.about_me, '')
 
     def test_default_team_number(self):
-        # Create a Mentor object without specifying a 'team_number'
         mentor = Mentor.objects.create(
             user=self.user,
             skills=['HTML']
         )
 
-        # Assert that the default value for 'team_number' is set
         self.assertEqual(mentor.team_number, 0)
