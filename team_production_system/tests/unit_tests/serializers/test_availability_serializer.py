1b5d8d914e7c8158b3325be244635ee418689215
Em Sparr <em.c.sparr@gmail.com>
2023-10-18 00:26:53 -0400

    tests: update tests

--- team_production_system/tests/unit_tests/serializers/test_availability_serializer.py
+++ team_production_system/tests/unit_tests/serializers/test_availability_serializer.py
@@ -1,18 +1,28 @@
 from django.test import TestCase
 from django.utils import timezone
 from rest_framework import serializers
+from django.http import HttpRequest
 from ....serializers import AvailabilitySerializerV2
+from ....models import Mentor, CustomUser
 
 
 class AvailabilitySerializerV2TestCase(TestCase):
+    def setUp(self):
+        self.user = CustomUser.objects.create_user(
+            username='testuser', password='testpass')
+        self.mentor = Mentor.objects.create(user=self.user)
+        self.request = HttpRequest()
+        self.request.user = self.user
+
     def test_validate(self):
-        serializer = AvailabilitySerializerV2()
+        serializer = AvailabilitySerializerV2(context={'request': self.request})
         start_time = timezone.now() + timezone.timedelta(hours=1)
         end_time = timezone.now() + timezone.timedelta(hours=2)
         data = {
             'start_time': start_time,
-            'end_time': end_time,
+            'end_time': end_time
         }
+
         # Test valid input data
         validated_data = serializer.validate(data)
         self.assertEqual(validated_data, data)
