b689cfbb1042d05d5cd097866c981f8cefeef01f
Em Sparr <em.c.sparr@gmail.com>
2023-10-18 00:02:42 -0400

    chore: realign urls

--- team_production_system/urls.py
+++ team_production_system/urls.py
@@ -5,8 +5,8 @@ from team_production_system import views
 urlpatterns = [
 
      # V1 API #
-      # User endpoints
-      path('myprofile/', views.UserProfile.as_view(), name='my-profile'),
+     # User endpoints
+     path('myprofile/', views.UserProfile.as_view(), name='my-profile'),
 
      # Mentor end points
      path('mentor/', views.MentorList.as_view(), name='mentor-list'),
@@ -24,31 +24,31 @@ urlpatterns = [
           name='mentee-info-update'),
 
       # Availability endpoints
-      path(
-            'availability/',
-            views.AvailabilityListCreateView.as_view(),
-            name='availability'
-      ),
-       path('availability/<int:pk>/', views.AvailabilityDeleteView.as_view(),
-           name='availability-delete'),
+     path(
+          'availability/',
+          views.AvailabilityListCreateView.as_view(),
+          name='availability'
+     ),
+     path('availability/<int:pk>/', views.AvailabilityDeleteView.as_view(),
+          name='availability-delete'),
  
      # Session endpoints
      path('session/', views.SessionView.as_view(), name='session'),
-      path('archivesession/', views.ArchiveSessionView.as_view(),
-           name='archive-session'),
-      path('sessionrequest/', views.SessionRequestView.as_view(),
-           name='session'),
-      path('sessionrequest/<int:pk>/', views.SessionRequestDetailView.as_view(),
-           name='session-detail'),
-      path('sessionsignuplist/', views.SessionSignupListView.as_view(),
-           name='session-signup-list'),
+     path('archivesession/', views.ArchiveSessionView.as_view(),
+          name='archive-session'),
+     path('sessionrequest/', views.SessionRequestView.as_view(),
+          name='session'),
+     path('sessionrequest/<int:pk>/', views.SessionRequestDetailView.as_view(),
+          name='session-detail'),
+     path('sessionsignuplist/', views.SessionSignupListView.as_view(),
+          name='session-signup-list'),
  
      # Notification endpoints
      path(
             'notificationsettings/<int:pk>/',
             views.NotificationSettingsView.as_view(),
             name='notification-settings'
-      ),
+     ),
 
      # V2 API #
      # Availability endpoints
