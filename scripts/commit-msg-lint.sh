340fa9203a8fe0895607a0390f9bcaf1419838aa
James Andrews <jbandrew14@gmail.com>
2023-10-29 18:55:43 -0400

    fix: commit script should now handle scope conditionally

--- scripts/commit-msg-lint.sh
+++ scripts/commit-msg-lint.sh
@@ -3,9 +3,10 @@
 commit_msg_file=$1
 commit_msg=$(cat $commit_msg_file)
 
-regex="^(feat|fix|docs|style|refactor|perf|test|build|ci|chore|revert): .+$"
+regex_without_scope="^(feat|fix|docs|style|refactor|perf|test|build|ci|chore|revert): .+$"
+regex_with_scope="^(feat|fix|docs|style|refactor|perf|test|build|ci|chore|revert)\([a-z]+\): .+$"
 
-if [[ ! $commit_msg =~ $regex ]]; then
+if [[ ! $commit_msg =~ $regex_with_scope ]] && [[ ! $commit_msg =~ $regex_without_scope ]]; then
     echo "Error: Commit message does not match the required format!"
     echo "Expected Format: <category(scope): description>"
     echo "Example: chore(test): setup test"
