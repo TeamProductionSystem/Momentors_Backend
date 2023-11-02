d03eed5fb133396ae9ba8f5933c657bc0a29a550
James Andrews <jbandrew14@gmail.com>
2023-11-02 10:27:19 -0400

    fix: removed deleted code that made it through merge/rebase

--- team_production_system/views/user_profile.py
+++ team_production_system/views/user_profile.py
@@ -1,12 +1,8 @@
-import boto3
-from django.conf import settings
 from rest_framework import generics, status
 from rest_framework.parsers import MultiPartParser
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
-from django.conf import settings
-from team_production_system.serializers import CustomUserSerializer
 from team_production_system.models import CustomUser
 from team_production_system.serializers import CustomUserSerializer
 
@@ -57,12 +53,9 @@ class UserProfile(generics.RetrieveUpdateDestroyAPIView):
 
         if 'profile_photo' in request.FILES:
             if user.profile_photo:
-                s3 = boto3.client('s3')
-                s3.delete_object(
-                    Bucket=settings.AWS_STORAGE_BUCKET_NAME, Key=user.profile_photo.name
-                )
                 # remove the file from storage
                 user.profile_photo.storage.delete(user.profile_photo.name)
+
             user.profile_photo = request.FILES['profile_photo']
 
         user.save()
